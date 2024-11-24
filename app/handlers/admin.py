ba4dc33fdb139886be849b50969263f9c7f1c36c
Anton Khorn <anton.pres@yandex.ru>
2024-11-24 19:29:07 +0300

    fix

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -5,4 +5,4 @@ async def admin_user(message: Message, bot: AsyncTeleBot):
     """
     You can create a function and use parameter pass_bot.
     """
-    await bot.send_message(message.chat.id, "Hello, admin!")
+    await bot.send_message(message.chat.id, "Hello, admin! Начало")
